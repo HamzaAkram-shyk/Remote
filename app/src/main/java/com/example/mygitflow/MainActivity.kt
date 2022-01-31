df28aab005d56391afe3c0879bc9052f5dede8ba
hamza-akram-sct <hamzaakram.shyk172gmail.com>
2022-01-31 16:05:24 +0500

    Add Profile

--- app/src/main/java/com/example/mygitflow/MainActivity.kt
+++ app/src/main/java/com/example/mygitflow/MainActivity.kt
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var profile: Profile
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        profile = Profile()
         println("Hello")
         println("This is My First Feature")
+        println("This is feature Two")
+        println("This is New Repo")
     }
 }
